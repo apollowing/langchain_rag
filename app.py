a1d74658aaff84cae150c0c6d6e59214e627902c
Michael <chankwm@sg.ibm.com>
2024-04-25 12:50:51 +0800

    added support for pdf and html files. chained_type of retriever to refine for better quality answers. changed folder structure. included dependencies in requirements.txt

--- app.py
+++ app.py
@@ -15,22 +15,26 @@
 # So you may have to adjust the prompt as necessary
 # Refer to documentation related with the model for more details
 
+# Useful references
+# - https://medium.com/@onkarmishra/using-langchain-for-question-answering-on-own-data-3af0a82789ed
+# - https://medium.com/ai-in-plain-english/using-langchain-chains-and-agents-for-llm-application-development-d538f6c70bc6
+
 from langchain.chains import RetrievalQA
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.manager import CallbackManager
 from langchain_community.llms import Ollama
-from langchain_community.llms.openai import OpenAI
+from langchain_openai import OpenAI
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader, BSHTMLLoader
+from langchain_community.document_loaders import TextLoader, BSHTMLLoader, PyPDFLoader
 from langchain.prompts import PromptTemplate
 import streamlit as st
 import os
 
-DOC_FOLDER = r".\diary"
-FILE_EXT = ".md"
-
+DOC_FOLDER = r".\docs"  # documents to be vectorized and indexed
+DB_FOLDER = r".\.db"    # chroma db
+FILE_EXT = (".md", ".html", ".pdf")  # file extensions supported
 # configuration of the LLM
 # todo: import this from a json file
 RAG_CONFIGS = [
@@ -89,11 +93,21 @@ def split_docs(vectorstore, path=DOC_FOLDER, file_ext=FILE_EXT):
                         # loader = BSHTMLLoader(filename)     # use beautiful soup loader if using .html
                         # if os.path.exists(filename): 
                         #     print(f"found {filename}")
-                        loader = TextLoader(filename, autodetect_encoding=True)
+                        if filename.endswith(".pdf"):
+                            loader = PyPDFLoader(filename)
+                        elif filename.endswith(".html"):
+                            loader = BSHTMLLoader(filename)
+                        else:
+                            loader = TextLoader(filename, autodetect_encoding=True)
+
                         doc = loader.load()
                     except UnicodeDecodeError as e:
                         print(f"Error decoding the file: {e}")
-
+                        continue
+                    except Exception as e:
+                        print(f"Error loading the file: {filename} - {e}")
+                        continue
+                        
                     i = 0
                     chunks = text_splitter.split_documents(doc)
                     print("number of chunks: ",chunks.__len__())
@@ -117,16 +131,30 @@ def main():
     
     loaded = st.session_state.get("loaded", False)
     if not loaded:
+
+        # Define custom CSS for word wrapping
+        custom_css = """
+        <style>
+            .streamlit-text-container {
+                white-space: pre-wrap;
+                font-size: 14px;
+            }
+        </style>
+        """
+
+        # Inject the custom CSS
+        st.markdown(custom_css, unsafe_allow_html=True)
+
         ## Add document along with unique IDs so we don't include the same documents again
         embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        vectorstore = Chroma(persist_directory=os.path.join(DOC_FOLDER, "db"),
+        vectorstore = Chroma(persist_directory=DB_FOLDER,
                             embedding_function=embedding)             
 
         docs = []
         ids = []
         docs, ids = split_docs(vectorstore)
         if not ids == []:
-            vectorstore = vectorstore.from_documents(documents=docs, embedding=embedding, ids=ids, persist_directory=os.path.join(DOC_FOLDER, "db"))
+            vectorstore = vectorstore.from_documents(documents=docs, embedding=embedding, ids=ids, persist_directory=DB_FOLDER)
             vectorstore.persist()
 
         retriever = vectorstore.as_retriever()
@@ -134,7 +162,7 @@ def main():
         # test_retrival = vectorstore.similarity_search_with_score("Who are my family?", 3)
         # print(f"test vector retrival: {test_retrival}")
 
-        if MODE == "local":
+        if MODE == "local_ollama":
             llm = Ollama(
                 base_url=CONFIG["base_url"],
                 model=CONFIG["model"],
@@ -148,7 +176,7 @@ def main():
                 base_url=CONFIG["base_url"],
                 model=CONFIG["model"],
                 openai_api_key=CONFIG["api_key"],
-                temperature=0.7,
+                temperature=0,
                 verbose=True,
                 callback_manager=CallbackManager(
                     [StreamingStdOutCallbackHandler()]),
@@ -170,13 +198,16 @@ def main():
                 )
         qa_chain = RetrievalQA.from_chain_type(
                     llm=llm,
-                    chain_type='stuff',
+                    chain_type="refine",
+                    # chain_type='stuff',
+                    # chain_type="map_reduce",                    
+                    # chain_type="map_rerank",                    
                     retriever=st.session_state["retriever"],
                     verbose=True,
-                    chain_type_kwargs={
-                        "verbose": True,
-                        "prompt": prompt,
-                    })
+                    # chain_type_kwargs={
+                    #     "verbose": True,
+                    #     "prompt": prompt,}
+        )
         
         st.session_state["qa_chain"] = qa_chain
         st.session_state["loaded"] = True
